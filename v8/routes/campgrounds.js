32a08c448d1acc45561359ddbc7200c1257b1688
Steven <stntsai93@gmail.com>
2020-09-08 17:53:06 +0800

    Users of commits feature updated

---
+++ v8/routes/campgrounds.js
@@ -0,0 +1,58 @@
+var express = require("express")
+var router = express.Router()
+var Campground = require("../models/campground")
+
+//show all campgrounds
+router.get("/", function(req, res) {
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds});
+        }
+    })
+})
+
+//create campgrounds
+router.post("/", isLoggedIn, function(req, res) {
+    var name = req.body.name
+    var image = req.body.image
+    var desc = req.body.description
+    var newCampground = { name: name, image: image, description: desc }
+        //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
+//NEW
+router.get("/new", isLoggedIn, function(req, res) {
+    res.render("campgrounds/new")
+})
+
+//SHOW
+router.get("/:id", function(req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("campgrounds/show", { campground: foundCampground })
+        }
+    })
+})
+
+//middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect("/login")
+}
+
+
+module.exports = router
