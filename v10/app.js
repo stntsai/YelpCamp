92f4b2265c31fe3a5486c2ce23b957b290ba7a8b
Steven <stntsai93@gmail.com>
2020-09-08 18:40:16 +0800

    Edit/Delete Campgrounds/Comments

---
+++ v10/app.js
@@ -0,0 +1,52 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seeds")
+
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index")
+
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"))
+// seedDB() //seed the database
+
+//Passport configuration
+app.use(require("express-session")({
+    secret: "I am the KING!",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user
+    next()
+})
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes)
+
+app.listen(3000, function() {
+    console.log("YelpCamp Server Is Listening")
+})
---
+++ v10/routes/campgrounds.js
@@ -0,0 +1,84 @@
+var express = require("express")
+var router = express.Router()
+var Campground = require("../models/campground")
+var middleware = require("../middleware")
+
+//show all campgrounds
+router.get("/", function(req, res) {
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("campgrounds/index", { campgrounds: allCampgrounds});
+        }
+    })
+})
+
+//create campgrounds
+router.post("/", middleware.isLoggedIn, function(req, res) {
+    var name = req.body.name
+    var image = req.body.image
+    var desc = req.body.description
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = { name: name, image: image, description: desc, author: author }
+        //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err)
+        } else {
+            // console.log(newlyCreated)
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
+//NEW
+router.get("/new", middleware.isLoggedIn, function(req, res) {
+    res.render("campgrounds/new")
+})
+
+//SHOW
+router.get("/:id", function(req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("campgrounds/show", { campground: foundCampground })
+        }
+    })
+})
+
+//Edit campground routes
+router.get("/:id/edit", middleware.checkCampgroundOwnership,function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        res.render("campgrounds/edit", {campground: foundCampground})
+    })
+})
+
+//Update campround routes
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect("/campgrounds")
+        }else{
+            res.redirect("/campgrounds/"+ req.params.id)
+        }
+    })
+})
+
+//Destroy campground routes
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findByIdAndDelete(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds")
+        }else{
+            res.redirect("/campgrounds")
+        }
+    })
+})
+
+module.exports = router
