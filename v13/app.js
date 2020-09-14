6729dd81eb6ea3a491a6ead81c063e15a6e64be4
Steven Tsai <stntsai93@gmail.com>
2020-09-14 23:00:21 +0800

    Update app.js

--- v13/app.js
+++ v13/app.js
@@ -42,7 +42,7 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-//fash configuration
+//flash configuration
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user
     res.locals.error = req.flash("error")
